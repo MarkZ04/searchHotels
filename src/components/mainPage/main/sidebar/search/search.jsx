c5c5905cd1991e5125de0f1e05659ac12037b25f
MarkZ04 <71509431+MarkZ04@users.noreply.github.com>
2022-06-08 17:04:56 +0300

    added input restrictions in the location field

--- src/components/mainPage/main/sidebar/search/search.jsx
+++ src/components/mainPage/main/sidebar/search/search.jsx
@@ -12,35 +12,82 @@ export const Search = (props) => {
 	let [numberOfDays, setNumberOfDays] = useState(1);
 
 	const setNewLocation = () => {
-		setLocation(locationValue.current.value);
-	};
+		const reg = /[\w\[\]=+)(.,><!?:;"'@#$%^&*~`}{]/i;
 
+		if (locationValue.current.value.match(reg) === null) {
+			setLocation(() => {
+				if (locationValue.current.value.length !== 0) {
+					locationValue.current.value =
+						locationValue.current.value[0].toUpperCase() +
+						locationValue.current.value.slice(1);
+
+					if (
+						locationValue.current.value[0] === ' ' ||
+						locationValue.current.value[0] === '-'
+					) {
+						locationValue.current.value = '';
+					}
+
+					// let i = locationValue.current.value
+					// 	.split('')
+					// 	.findIndex((el) => el === ' ');
+					// console.log(i);
+					// if (locationValue.current.value[i + 1] === ' ') {
+					// 	return (
+					// 		locationValue.current.value.slice(0, i + 1) +
+					// 		locationValue.current.value.slice(i + 2)
+					// 	);
+					// }
+					switch (
+						locationValue.current.value[locationValue.current.value.length - 2]
+					) {
+						case ' ': // enter after space
+							if (locationValue.current.value.slice(-1).match(/\s/) === null) {
+								return (
+									locationValue.current.value.slice(0, -1) +
+									locationValue.current.value.slice(-1).toUpperCase()
+								);
+							}
+							return locationValue.current.value.slice(0, -1);
+
+						case '-': // enter after '-'
+							if (locationValue.current.value.slice(-1).match(/-/) === null) {
+								return (
+									locationValue.current.value.slice(0, -1) +
+									locationValue.current.value.slice(-1).toUpperCase()
+								);
+							}
+							return locationValue.current.value.slice(0, -1);
+					}
+					return locationValue.current.value;
+				}
+				return locationValue.current.value;
+			});
+		}
+	};
 	const setNewCheckIn = () => {
 		setCheckIn(checkInValue.current.value);
 	};
-
 	const setNewNumberOfDays = () => {
 		setNumberOfDays(numberOfDaysValue.current.value);
 	};
 
 	const onSearch = () => {
-		console.log(location); // исправить заглавную букву
-		let cityName = location[0].toUpperCase() + location.slice(1).toLowerCase();
-
-		props.setCityNameAC(cityName);
+		props.setCityNameAC(location);
 		props.setCheckInAC(checkIn);
 		props.setCheckOutAC(props.dateCheckOut(checkIn, numberOfDays));
 	};
 
 	return (
 		<div className={`${style.search}`}>
-			<form action='#' className={`card ${style.card}`}>
+			<div className={`card ${style.card}`}>
 				<p className={`${style.subtitle}`}>Локация</p>
 				<input
 					ref={locationValue}
 					className={`input`}
 					onChange={setNewLocation}
 					value={location}
+					type={'text'}
 				/>
 
 				<p className={`${style.subtitle}`}>Дата заселения</p>
@@ -58,12 +105,15 @@ export const Search = (props) => {
 					className={`input`}
 					onChange={setNewNumberOfDays}
 					value={numberOfDays}
+					type={'number'}
+					min={1}
+					max={2913747}
 				/>
 
 				<button className={`btn`} onClick={onSearch} type='submit'>
 					Найти
 				</button>
-			</form>
+			</div>
 		</div>
 	);
 };
